af95f9384705bcf3db1e52894c9e439f5c6f1962
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-02-15 18:27:21 -0600

    Add chatbot functionality to the project

---
+++ chatbot.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI()
+
+
+def get_response(
+    prompt: str,
+    system: str = "Eres una asistente poética, hábil para explicar conceptos complejos de programación con un toque creativo.",
+) -> str:
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": system,
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+    )
+
+    return completion.choices[0].message.content
+
+
+def main():
+    system = input("System: ")
+    print("=== Chatbot ===")
+    while True:
+        prompt = input("Prompt: ")
+        response = get_response(prompt, system)
+        print(f"IA: {response}")
+        continue_loop = input("Continuar? (s/n):")
+        if continue_loop != "s":
+            break
+
+
+if __name__ == "__main__":
+    main()
